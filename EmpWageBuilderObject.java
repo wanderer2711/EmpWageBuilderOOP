aed0b952dbd9b0cc0c40370e81eb6d2aec3e33d5
suhridh <ssg.2711@gmail.com>
2021-09-18 00:07:29 +0530

    added UC4

--- EmpWageBuilderObject.java
+++ EmpWageBuilderObject.java
@@ -10,10 +10,15 @@ public class EmpWageBuilderObjectUC2 {
         int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
-        if (empCheck == IS_FULL_TIME) {
-            empHrs = 8
-        } else {
-            empHrs = 4;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage: " + empWage);
